62ffd1d951e570a7be12ed20f72f5daede47a9f0
epent <helene.pentina@gmail.com>
2022-05-14 17:41:24 +0300

    add hideWelcomePart

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -14,11 +14,24 @@ function addTask() {
   if (text === "") {
     alert("Input cannot be empty");
   } else {
-    createTask();
+    hideWelcomePart();
+    createTask(text);
   }
 }
 
-function createTask() {
+function hideWelcomePart() {
+  const addButton = document.querySelector("#add-task-button");
+  const triangle = document.querySelector("#triangle");
+  const box = document.querySelector("#box");
+  const welcome = document.querySelector("#welcome-sentence");
+
+  addButton.classList.remove("hithere");
+  triangle.classList.add("hidden");
+  box.classList.add("hidden");
+  welcome.classList.add("hidden");
+}
+
+function createTask(text) {
   //create elements
   const divElm = document.createElement("div");
   const liElm = document.createElement("li");
