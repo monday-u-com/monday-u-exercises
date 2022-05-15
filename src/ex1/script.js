b4f3111fdaa5e16c3838044ccaad6610dff7b537
liortlv <99750449+liortlv@users.noreply.github.com>
2022-05-15 18:39:22 +0300

    Add delete button with basic design too

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,18 +1,41 @@
 const addButton = document.querySelector('.add-task');
 const taskInput = document.querySelector('#task-text');
-const tasksContainer = document.querySelector('.tasks-container');
+const allTasksContainer = document.querySelector('.all-tasks-container');
 
 addButton.addEventListener('click', () => {
     addTask();
 })
 
 function addTask() {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add('task-container');
+    allTasksContainer.append(taskContainer);
+
     const newTask = document.createElement("div");
     newTask.textContent = taskInput.value;
     newTask.classList.add('task');
-    tasksContainer.append(newTask);
+    taskContainer.append(newTask);
     newTask.addEventListener('click', () => {
         alert(newTask.textContent);
     })
+
+    const deleteTask = document.createElement("div");
+    deleteTask.textContent = 'D';
+    deleteTask.classList.add('delete');
+    deleteTask.addEventListener('click', () => {
+        taskContainer.remove();
+    })
+    taskContainer.append(deleteTask);
+
+    taskContainer.addEventListener('mouseover', () => {
+        deleteTask.style.display = 'flex';
+        newTask.style.width = '26rem';
+    })
+
+    taskContainer.addEventListener('mouseout', () => {
+        deleteTask.style.display = 'none';
+        newTask.style.width = '29.5rem';
+    })
+
 }
 
