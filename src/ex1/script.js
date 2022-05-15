4eb062f32e6a88a6df77fb9d7f8864fc9bfecfbf
matan-shuster <shusmatan@gmail.com>
2022-05-15 21:48:44 +0300

    Added dynamic todo List, need to fix CSS

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,8 +1,10 @@
 document.getElementById("addTodo").addEventListener("click", function() {
-  let todo = document.getElementById("todo").value;
+  let todo = document.getElementById("newTodo").value;
   let todoList = document.getElementById("todoList");
   let li = document.createElement("li");
   li.innerHTML = todo;
+  li.innerHTML += "<input type='checkbox' name='selectTodo' value='" + todo + "'>";
   todoList.appendChild(li);
-
+  document.getElementById("newTodo").value = "";
 });
+
