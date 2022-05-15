fa59794d2f91819c6c94bcb6ca22d63782f3e56e
nadav-av <avr.nadav@gmail.com>
2022-05-15 12:21:36 +0300

    adds show and hid in hover for del and check task

--- src/ex1/scripts/script.js
+++ src/ex1/scripts/script.js
@@ -39,14 +39,18 @@ function onAddTaskClick(e) {
 
   const completeTaskButton = document.createElement("button");
   completeTaskButton.classList.add("task-complete-btn");
+  completeTaskButton.classList.add("hide");
   completeTaskButton.innerHTML = '<i class="fas fa-check"></i>';
   taskDiv.appendChild(completeTaskButton);
 
   const removeTaskButton = document.createElement("button");
   removeTaskButton.classList.add("task-remove-btn");
+  removeTaskButton.classList.add("hide");
   removeTaskButton.innerHTML = '<i class="fas fa-trash"></i>';
   taskDiv.appendChild(removeTaskButton);
 
+  taskDiv.addEventListener("mouseover", onTaskMouseOver);
+  taskDiv.addEventListener("mouseleave", onTaskMouseOut);
   tasksList.appendChild(taskDiv);
   toggleEmptyMsg();
   taskInput.value = "";
@@ -110,4 +114,21 @@ function onSearchInputKeyUp(e) {
 
 function onTaskClick(e) {
   alert(e.target.innerText);
+}
+
+function onTaskMouseOver(e) {
+  const completeTaskButton = e.target.querySelector(".task-complete-btn");
+  completeTaskButton.classList.remove("hide");
+  const removeTaskButton = e.target.querySelector(".task-remove-btn");
+  removeTaskButton.classList.remove("hide");
+}
+
+function onTaskMouseOut(e) {
+  console.log(e.target);
+  const completeTaskButton = e.target.querySelector(".task-complete-btn");
+  console.log("completeTaskButton:", completeTaskButton);
+  completeTaskButton.classList.add("hide");
+  const removeTaskButton = e.target.querySelector(".task-remove-btn");
+  console.log("removeTaskButton:", removeTaskButton);
+  removeTaskButton.classList.add("hide");
 }
