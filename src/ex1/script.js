eb6bd91a70d43b5f2f06a6c339fe42d61a8ef3f4
Alex Levenberg <zeronid@gmail.com>
2022-05-16 11:27:37 +0300

    ADD: added new features 1. Delete button. 2. Input clear on add. 3. Input validation.

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,8 +1,26 @@
 function addTask() {
+
+    if (!document.getElementById("new-task-input").value.length) {
+        alert("Please input a value")
+        return
+    }
+
     const newTaskText = document.getElementById("new-task-input").value
-    const newTaskDiv = document.createElement("div");
-    const newTextDiv = document.createTextNode(newTaskText);
+    const newTaskDiv = document.createElement("div"); //Task
+    const newTaskDivContainer = document.createElement("div"); //Task Container
+    const newTextDiv = document.createTextNode(newTaskText); //Text
+    const deleteButton = document.createElement("button"); //Button
+    deleteButton.classList.add("delete-button")
+    deleteButton.addEventListener("click", () => {
+        newTaskDivContainer.remove()
+    })
     newTaskDiv.appendChild(newTextDiv);
+    newTaskDiv.addEventListener("click", () => alert(newTaskDiv.innerHTML))
     newTaskDiv.classList.add("task")
-    document.getElementById("task-container").appendChild(newTaskDiv)
+    newTaskDivContainer.classList.add("new-task-div-container")
+    newTaskDivContainer.appendChild(newTaskDiv)
+    newTaskDivContainer.appendChild(deleteButton)
+    document.getElementById("task-container").appendChild(newTaskDivContainer)
+
+    document.getElementById("new-task-input").value = ""
 }
