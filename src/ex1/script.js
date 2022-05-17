b45cba146d2a4084d9d0cf0472c0b852410f7c8c
Anna Shishkova <asshishkova@gmail.com>
2022-05-17 14:46:36 +0300

    Clear all tasks and update the amount in summary

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,4 +1,4 @@
-const todoList = document.getElementsByClassName('todo-list')[0];
+const todoList = document.getElementsByClassName("todo-list")[0];
 const todosArray = ['one', 'two', 'three', 'four', 'five'];
 
 todoList.innerHTML = "";
@@ -20,8 +20,20 @@ todosArray.forEach(todoText => {
   todoList.appendChild(listItem);
 });
 
-const todos = document.querySelectorAll('.todo-text');
-const deleteButtons = document.querySelectorAll('.todo-list .remove-todo-button');
+const todos = document.querySelectorAll(".todo-text");
+const deleteButtons = document.querySelectorAll(".todo-list .remove-todo-button");
+const clearAllButton = document.getElementById("clear-all-button");
+const amountInfo = document.getElementById("amount-info");
+
+function amountOfTasksMessage() {
+  let tasks = "tasks";
+  if (todosArray.length === 1) {
+    tasks = "task";
+  }
+  amountInfo.textContent = `You have ${todosArray.length} pending ${tasks}`;
+}
+
+amountOfTasksMessage();
 
 todos.forEach (todo => {
   todo.addEventListener('click', ({target}) => {
@@ -44,4 +56,13 @@ function onDeleteButtonClicked(clickedButton) {
   const index = Array.prototype.indexOf.call(todoList.childNodes, clickedButton.parentElement);
   todosArray.splice(index, 1);
   clickedButton.parentElement.remove();
+  amountOfTasksMessage();
+}
+
+clearAllButton.addEventListener('click', onClearAllButtonClicked);
+
+function onClearAllButtonClicked() {
+  todoList.innerHTML = "";
+  todosArray.length = 0;
+  amountOfTasksMessage();
 }
