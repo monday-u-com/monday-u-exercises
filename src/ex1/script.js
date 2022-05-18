151ba4f85fc63ab4a794d030cd7ff688c633e12d
matan-shuster <82678238+matan-shuster@users.noreply.github.com>
2022-05-18 21:12:44 +0300

    trial 2

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,10 +1,14 @@
+let count = 0;
 document.getElementById("addTodo").addEventListener("click", function() {
   let todo = document.getElementById("newTodo").value;
   let todoList = document.getElementById("todoList");
   let li = document.createElement("li");
-  li.innerHTML = todo;
-  li.innerHTML += "<input type='checkbox' name='selectTodo' value='" + todo + "'>";
+  li.id = 'todo' + count;
+  li.innerHTML = `<input type='checkbox' name='selectTodo' value="${todo}">`;
+  li.innerHTML += todo;
   todoList.appendChild(li);
   document.getElementById("newTodo").value = "";
+  count++;
 });
+
 
