53985052951732d81315b7703173af93a467b055
Ori Marom <ori.marom10@gmail.com>
2022-05-18 15:38:24 +0300

    get li element inner text

--- src/ex1/README.md
+++ src/ex1/README.md
@@ -20,7 +20,7 @@ The requirements:
 - [x] Add a button ("Add task")
 - [x] When "Add task" button is clicked, add a task to the task list
 - [x] Change the background and cursor type of an item when hovered on
-- [ ] When a list item is clicked, show 'alert' (**Hint**: check the [alert](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert) method) with the item name
+- [] When a list item is clicked, show 'alert' (**Hint**: check the [alert](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert) method) with the item name
 - [x] Delete an item when "Delete" button is clicked
 
 
@@ -37,4 +37,4 @@ Bonus
 
 When you finish, it should look like this:
 
-![](https://res.cloudinary.com/practicaldev/image/fetch/s--pyyuGSZ9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/o96lsrld21tk232kidu4.png)
+![x]](https://res.cloudinary.com/practicaldev/image/fetch/s--pyyuGSZ9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/o96lsrld21tk232kidu4.png)
--- src/ex1/script.js
+++ src/ex1/script.js
@@ -6,62 +6,76 @@ const clearAllBtn = document.getElementById("clearAll-btn");
 const sortBtn = document.getElementById("sort-btn");
 const inputBox = document.querySelector(".inputField input");
 const svgContainer = document.getElementById("svg");
+const todoElement = document.getElementById("list-element");
 
 const animItem = bodymovin.loadAnimation({
-    wrapper:svgContainer,
-    animType: 'svg',
-    loop: false,
-    autoplay: false,
-    path: "https://assets4.lottiefiles.com/private_files/lf30_5aubt2fy.json"
-})
+  wrapper: svgContainer,
+  animType: "svg",
+  loop: false,
+  autoplay: false,
+  path: "https://assets4.lottiefiles.com/private_files/lf30_5aubt2fy.json",
+});
 
 const pendingTasksCount = document.querySelector(".pendingTasksCount");
-
-
 
 //Event Listeners
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
-todoInput.addEventListener("keyup" , todoEnter);
-todoList.addEventListener("click" , deleteTask);
-sortBtn.addEventListener("click" ,sortTasks);
-clearAllBtn.addEventListener("click" , clearAll); 
+todoInput.addEventListener("keyup", todoEnter);
+
+todoList.addEventListener("click", deleteTask);
+sortBtn.addEventListener("click", sortTasks);
+clearAllBtn.addEventListener("click", clearAll);
+
+todoElement.addEventListener("click", alertTask) ;
+
+function alertTask(e) {
+    const item = e.target;
+    
+  
+    if (item.classList[0] === "todo") {
+      const todo = item.parentElement;
+      console.log(item.innerText)
+    }
+  }
 
 
-
+/* function alertTodo() {
+  console.log("hi");
+  alert("hi");
+} */
 
 //Functions
 
 //todo input styling
 todoInput.onkeydown = () => {
-    if (todoInput.value.length){
+  if (todoInput.value.length) {
     inputBox.classList.add("inactive");
     todoButton.classList.add("inactive");
-    } 
+  }
 
-    if (!todoInput.value.length && !todoList.childElementCount){
-        inputBox.classList.remove("inactive");
-        todoButton.classList.remove("inactive");
-        } 
-}
-
+  if (!todoInput.value.length && !todoList.childElementCount) {
+    inputBox.classList.remove("inactive");
+    todoButton.classList.remove("inactive");
+  }
+};
 
 //add todo to the todo list
 function addTodo(event) {
-   
-    if (todoInput.value.trim() == 0){
-        return launch_toast()
-    }
-    event.preventDefault();
-  
+  if (todoInput.value.trim() == 0) {
+    return launch_toast();
+  }
+  event.preventDefault();
+
   //Todo li
-  animItem.goToAndPlay(0,true);
+  animItem.goToAndPlay(0, true);
   const todoLi = document.createElement("li");
   todoLi.classList.add("todo");
   todoLi.innerText = todoInput.value;
 
   //Add todo to local storage
-  saveLocalTodos(todoInput.value);
+  todos = saveLocalTodos(todoInput.value);
+  /* todoLi.setAttribute('id', "liNum" + todos.indexOf(todo)) */
   //trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
@@ -69,179 +83,166 @@ function addTodo(event) {
   todoLi.appendChild(trashButton);
   //append to list
   todoList.appendChild(todoLi);
+  todoLi.setAttribute("id", "liNum" + todoList.childElementCount);
+
   //Clear Todo Input value
   todoInput.value = "";
   inputBox.classList.add("inactive");
 }
 
 //add todo when pressing enter key
-function todoEnter(event){  
-    
-    if (event.keyCode == 13) {
-       
-    if (todoInput.value.trim() == 0){
-        return launch_toast()
+function todoEnter(event) {
+  if (event.keyCode == 13) {
+    if (todoInput.value.trim() == 0) {
+      return launch_toast();
     }
     addTodo(event);
-       
-      }
+  }
 }
 
 //delete a todo from the list
 function deleteTask(e) {
-    const item = e.target;
-    
-    if(item.classList[0] === "trash-btn"){
-       const todo = item.parentElement;
-       todo.classList.add("fall");
-       removeLocalTodos(todo);
-       setTimeout(function () {
-        //delay the remove animation
-        todo.remove();
-      }, 500);
-                
-    }
+  const item = e.target;
 
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    removeLocalTodos(todo);
+    setTimeout(function () {
+      //delay the remove animation
+      todo.remove();
+    }, 500);
+  }
 }
 
 //clear all button clicked
 function clearAll(e) {
-    const item = e.target;
-    if(item.classList[0] === "clearAllBtn"){
-      todoList.classList.add("fall");
-      
-       setTimeout(function () {
-        //delay the remove animation
-        todoList.innerHTML = '';
-        localStorage.clear();
-        todoList.classList.remove("fall");
-      }, 500);
-       pendingTasksCount.textContent = 0;
-       if (!todoInput.value.length){
-        inputBox.classList.remove("inactive");
-        todoButton.classList.remove("inactive");
-        }
-        sortBtn.textContent= 'Sort' 
+  const item = e.target;
+  if (item.classList[0] === "clearAllBtn") {
+    todoList.classList.add("fall");
+
+    setTimeout(function () {
+      //delay the remove animation
+      todoList.innerHTML = "";
+      localStorage.clear();
+      todoList.classList.remove("fall");
+    }, 500);
+    pendingTasksCount.textContent = 0;
+    if (!todoInput.value.length) {
+      inputBox.classList.remove("inactive");
+      todoButton.classList.remove("inactive");
     }
-    
+    sortBtn.textContent = "Sort";
+  }
 }
 
 //return values from local storage as an array
 function checkLocalStorage() {
-    let todos;
-    if(localStorage.getItem("todos") === null){
-        todos = [];
-
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    return todos;
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  return todos;
 }
 
 //save data to local storage
-function saveLocalTodos(todo){
-  
-    let todos =checkLocalStorage();
-    todos.push(todo);
-    localStorage.setItem("todos",JSON.stringify(todos));
-    pendingTasksCount.textContent = todos.length;
-   
-    
+function saveLocalTodos(todo) {
+  let todos = checkLocalStorage();
+  /* todoLi.setAttribute('id', "liNum" + todos.indexOf(todo)) */
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  pendingTasksCount.textContent = todos.length;
 }
-
 
 //get and show the data from local storage after refreshing the page
 function getTodos() {
-    //is the storage empty?
-    let todos = checkLocalStorage();
+  //is the storage empty?
+  let todos = checkLocalStorage();
 
-    todos.forEach(function(todo){
+  todos.forEach(function (todo) {
+    const todoLi = document.createElement("li");
+    todoLi.classList.add("todo");
+    todoLi.innerText = todo;
+    todoLi.setAttribute("id", "liNum" + todos.indexOf(todo));
+
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoLi.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoLi);
+    //Clear Todo Input value
+    todoInput.value = "";
+  });
+  pendingTasksCount.textContent = todos.length;
+  if (todos.length) {
+    inputBox.classList.add("inactive");
+    todoButton.classList.add("inactive");
+  }
+}
+
+// remove element from local storage
+function removeLocalTodos(todo) {
+  let todos = checkLocalStorage();
+  const todoIndex = todo.innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  pendingTasksCount.textContent = todos.length;
+  if (todos.length == 0) {
+    inputBox.classList.remove("inactive");
+    todoButton.classList.remove("inactive");
+    sortBtn.textContent = "Sort";
+  }
+}
+
+//sort function in ascending and descending
+function sortTasks() {
+  let todos = checkLocalStorage();
+  if (!todos.length) {
+    return;
+  }
+  if (todoList.classList.contains("ascending")) {
+    todos.sort(); /* ((a, b) => a.localeCompare(b)) */
+
+    sortBtn.textContent = "Sort 🡱";
+
+    todoList.classList.remove("ascending");
+    todoList.classList.add("descending");
+  } else {
+    todos.reverse();
+    todoList.classList.remove("descending");
+    todoList.classList.add("ascending");
+    sortBtn.textContent = "Sort 🡳";
+  }
+
+  todoList.innerHTML = "";
+  localStorage.setItem("todos", JSON.stringify(todos));
+  todos.forEach(function (todo) {
     const todoLi = document.createElement("li");
     todoLi.classList.add("todo");
     todoLi.innerText = todo;
 
-  //trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-btn");
-  todoLi.appendChild(trashButton);
-  //append to list
-  todoList.appendChild(todoLi);
-  //Clear Todo Input value
-  todoInput.value = "";
-     }    );
-     pendingTasksCount.textContent = todos.length;
-     if(todos.length){
-        inputBox.classList.add("inactive");
-        todoButton.classList.add("inactive");
-       }
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoLi.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoLi);
+    //Clear Todo Input value
+    todoInput.value = "";
+  });
 }
-
-// remove element from local storage
-function removeLocalTodos(todo){
-    let todos = checkLocalStorage();
-    const todoIndex = todo.innerText;
-    todos.splice(todos.indexOf(todoIndex), 1);
-    localStorage.setItem("todos" , JSON.stringify(todos));
-    pendingTasksCount.textContent = todos.length;
-    if (todos.length == 0){
-        inputBox.classList.remove("inactive");
-        todoButton.classList.remove("inactive");
-        sortBtn.textContent= 'Sort' 
-    }
-   
-}
-
-//sort function in ascending and descending
- function sortTasks() {
-   
-    let todos = checkLocalStorage();
-    if (!todos.length){
-        return;
-    }
-   if (todoList.classList.contains("ascending")) {
-    todos.sort()/* ((a, b) => a.localeCompare(b)) */
-    
-    sortBtn.textContent= 'Sort 🡱'
-
-
-    todoList.classList.remove("ascending")
-    todoList.classList.add("descending")
-   } else {
-    todos.reverse();
-    todoList.classList.remove("descending")
-    todoList.classList.add("ascending")
-    sortBtn.textContent= 'Sort 🡳'
-   }
-    
-
-    todoList.innerHTML = '';
-    localStorage.setItem("todos" , JSON.stringify(todos));
-    todos.forEach(function(todo){
-        const todoLi = document.createElement("li");
-        todoLi.classList.add("todo");
-        todoLi.innerText = todo;
-    
-      //trash button
-      const trashButton = document.createElement("button");
-      trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
-      trashButton.classList.add("trash-btn");
-      todoLi.appendChild(trashButton);
-      //append to list
-      todoList.appendChild(todoLi);
-      //Clear Todo Input value
-      todoInput.value = "";
-         }    );
-    
-  
-} 
-
 
 function launch_toast() {
-    
-    var x = document.getElementById("toast")
-    x.className = "show";
-    //clearInterval()
-    clearTimeout()
-    setTimeout(function(){ x.className = x.className.replace("show", ""); },1800);
+  var x = document.getElementById("toast");
+  x.className = "show";
+  //clearInterval()
+  clearTimeout();
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 1800);
 }
