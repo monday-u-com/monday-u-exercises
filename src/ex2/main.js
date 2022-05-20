9b3fbe9d1361d66be11c5d8ab5d1bfe0b30ca8e5
Vlad Mystetskyi <vlad@monday.com>
2022-05-20 09:24:50 +0300

    feat: add second exercise

---
+++ src/ex2/main.js
@@ -0,0 +1,10 @@
+// Implement the `Main` class here
+
+
+const main = new Main();
+
+document.addEventListener("DOMContentLoaded", function () {
+    // you should create an `init` method in your class
+    // the method should add the event listener to your "add" button
+    main.init();
+});
