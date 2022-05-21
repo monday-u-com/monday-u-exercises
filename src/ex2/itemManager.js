d14b4c87da6fb4d582bede3f10e2acffc70b4918
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-05-21 06:36:46 +0300

    feat:ItemManager addTask

--- src/ex2/itemManager.js
+++ src/ex2/itemManager.js
@@ -3,7 +3,29 @@ class ItemManager {
         this.taskList = [];
     }
 
-    
+    addTask(text){
+        //check if is int (pokemon)
+        const newTaskList=[...this.getTaskList()];
+        newTaskList.push(text);
+        this.setTaskList(newTaskList);
+    }
+
+    removeTask(text){
+        const newTaskList=[...this.getTaskList()];
+        const index=newTaskList.indexOf(text);
+        if (index !== -1) {
+            newTaskList.splice(index, 1);
+            this.setTaskList(newTaskList);
+            console.log('Item removed from list');
+        }
+        else console.log('removeFromTaskList ERROR!');
+    }
+
+    catchPokemon(number){
+        //URL:`https://pokeapi.co/api/v2/pokemon/${number}/`
+        //API GET request
+        //return {isPokemon:bool,names:[]} array of names
+    }
 
     getTaskList() {
         return this.taskList;
