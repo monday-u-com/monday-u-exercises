f5c331b8bee828ac648cdc876e91aca300b1c081
epent <helene.pentina@gmail.com>
2022-05-21 17:56:21 +0300

    add add/remove/render item to ItemManager

---
+++ src/ex2/ItemManager.js
@@ -0,0 +1,72 @@
+class ItemManager {
+  constructor() {
+    this.itemList = [];
+    this.addItem = this.addItem.bind(this);
+    this.renderItems = this.renderItems.bind(this);
+    this.createItem = this.createItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  addItem() {
+    const item = document.querySelector("#list-item-input").value;
+
+    this.itemList.push(item);
+    this.renderItems();
+  }
+
+  removeItem(item) {
+    const itemIndex = this.itemList.findIndex((listItem) => {
+      return listItem === item;
+    });
+
+    this.itemList.splice(itemIndex, 1);
+    renderItems();
+  }
+
+  renderItems() {
+    const list = document.querySelector("#list");
+    list.innerHTML = "";
+
+    this.itemList.forEach((item) => {
+      const itemNode = this.createItem(item);
+
+      list.appendChild(itemNode);
+    });
+  }
+
+  createItem(item) {
+    //create elements
+    const divElm = document.createElement("div");
+    const liElm = document.createElement("li");
+    const deleteButton = document.createElement("button");
+
+    // style  elements
+    divElm.classList.add("div-item");
+    // divElm.classList.add("grow");
+    liElm.classList.add("list-item");
+    deleteButton.classList.add("list-item-delete-button");
+
+    //add textNodes
+    liElm.appendChild(document.createTextNode(item));
+    deleteButton.appendChild(document.createTextNode("X"));
+
+    //add ids
+    const itemId = item.split(" ").join("-");
+    divElm.setAttribute("id", `${itemId}`);
+    deleteButton.setAttribute("id", `${itemId}-delete`);
+
+    //append <li> and <button> to <div>
+    divElm.appendChild(liElm);
+    divElm.appendChild(deleteButton);
+
+    //add clickListener to button
+    deleteButton.addEventListener("click", this.removeItem);
+
+    //clear input
+    document.querySelector("#list-item-input").value = "";
+
+    return divElm;
+  }
+}
+
+export const itemManager = new ItemManager();
