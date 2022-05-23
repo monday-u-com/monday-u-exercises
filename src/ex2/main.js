2c0899203f13669302153e389d5e55b20c3ca1cc
hhasona <hhasona@gmail.com>
2022-05-23 12:41:24 +0300

    ex2 starts

--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,10 +1,9 @@
 // Implement the `Main` class here
-
-
-const main = new Main();
+console.log("exercise 2 started")
+const main = new Main()
 
 document.addEventListener("DOMContentLoaded", function () {
-    // you should create an `init` method in your class
-    // the method should add the event listener to your "add" button
-    main.init();
-});
+  // you should create an `init` method in your class
+  // the method should add the event listener to your "add" button
+  main.init()
+})
