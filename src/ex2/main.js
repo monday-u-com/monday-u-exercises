eb187db6d2eb94cb2890d263ecb210a5da0c6a0b
Barak Maron <Barak062@gmail.com>
2022-05-24 21:40:34 +0300

    feat: adds notes

--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,64 +1,69 @@
 
 // Implement the `Main` class here
 
-class Main
-{
-    constructor()
-    {
+class Main {
+    constructor() {
         this.dom_manager = new DomManager();
         this.item_manager = new ItemManager();
     }
-
-    init()
-    {
+    /**
+     * init all event listeners
+     */
+    init() {
         this.dom_manager.add_task_button.addEventListener("click", () => {
             this.AddButtonCallBack();
         });
 
         // on enter key press event
-        this.dom_manager.add_task_button.addEventListener("keypress", (event)=> {
+        this.dom_manager.task_input.addEventListener("keypress", (event) => {
             this.dom_manager.AddNewTaskByKeyPress(event);
         });
 
         this.dom_manager.clear_all_button.addEventListener("click", () => {
-            this.dom_manager.ClearAllTasks();
+            this.item_manager.ClearArray();
+            this.RerenderFunctionWrapper();
         });
 
-        this.dom_manager.sort_by_name_button.addEventListener("click", () =>
-        {
-            this.dom_manager.SortByName();
+        this.dom_manager.sort_by_name_button.addEventListener("click", () => {
+            this.item_manager.SortArrayByName();
+            this.RerenderFunctionWrapper();
         });
     }
-
-    async AddButtonCallBack()
-    {
+    /**
+     * add button call back function
+     */
+    async AddButtonCallBack() {
         // check if input is not empty
-        if(this.dom_manager.task_input.value)
-        {
+        if (this.dom_manager.task_input.value) {
             // clear error message
             this.dom_manager.ClearErrorEmptyTask();
             // send the todo to item manager
-            const add_promise = Promise.resolve(this.item_manager.AddTodo(this.dom_manager.task_input.value));
+            const add_promise = Promise.resolve(this.item_manager.AddTask(this.dom_manager.task_input.value));
             // wait to get response before re-rendering
             await add_promise;
-            this.dom_manager.RenderDomFromArray(this.item_manager.todos, (event) => {
-                this.RemoveTodo(event);
-            });
+            this.RerenderFunctionWrapper();
         }
         else
             this.dom_manager.ShowErrorEmptyTaskInput(); // set empty error message
     }
-
-    RemoveTodo(event)
-    {
+    /**
+     * removes task from array
+     * @param {Event} event click event object
+     */
+    RemoveTodo(event) {
         const task_id = event.currentTarget.id;
-        this.item_manager.RemoveTodo(task_id);
-        this.dom_manager.RenderDomFromArray(this.item_manager.todos, (event) => {
-                this.RemoveTodo(event);
+        this.item_manager.RemoveTask(task_id);
+        this.RerenderFunctionWrapper();
+    }
+    /**
+     * function to wrap the re-render
+     */
+    RerenderFunctionWrapper()
+    {
+        this.dom_manager.RenderDomFromArray(this.item_manager.tasks, (event) => {
+            this.RemoveTodo(event);
         });
     }
-
-
 }
 
 
