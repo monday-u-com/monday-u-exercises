60e3598e90cc885ec08c16ae05f4436037134cf2
Anna Shishkova <asshishkova@gmail.com>
2022-05-26 14:04:54 +0300

    Refactor ItemManager

--- src/ex2/item-manager.js
+++ src/ex2/item-manager.js
@@ -4,44 +4,44 @@ const sortedDesc = Symbol("sortedDesc");
 
 export class ItemManager {
   init() {
-    this.todosArray = [
+    this.items = [
       {text: 'Walk the dog', isNew: false},
       {text: 'Take a shower', isNew: false},
       {text: 'Feed the baby', isNew: false},
       {text: 'Wash the dishes', isNew: false}
     ]
-    this.isSorted = unsorted;
-    return this.todosArray;
+    this.sortOrder = unsorted;
+    return this.items;
   }
 
-  deleteItem(index) {
-    this.todosArray.splice(index, 1);
-    return this.todosArray;
-  }
-
-  addItem(newItemText) {
-    const existingItemIndex = this.todosArray.findIndex(item => item.text === newItemText);
-    if (existingItemIndex > -1) {
-      this.todosArray[existingItemIndex].isNew = true;
+  addItem(text) {
+    const itemIndex = this.items.findIndex(item => item.text === text);
+    if (itemIndex > -1) {
+      this.items[itemIndex].isNew = true;
     } else {
-      this.todosArray.push({text: newItemText, isNew: true });
+      this.items.push({text: text, isNew: true });
     }
-    this.isSorted = unsorted;
-    return this.todosArray;
-  }
-
-  sortItems(){
-    if (this.isSorted === unsorted || this.isSorted === sortedDesc) {
-      this.todosArray.sort( (a, b) => a.text.localeCompare(b.text) );
-      this.isSorted = sortedAsc;
-    } else {
-      this.todosArray.reverse();
-      this.isSorted = sortedDesc;
-    }
-    return this.todosArray;
+    this.sortOrder = unsorted;
+    return this.items;
   }
 
   markItemAsOld(item){
     item.isNew = false;
+  }
+
+  deleteItem(index) {
+    this.items.splice(index, 1);
+    return this.items;
+  }
+
+  sortItems(){
+    if (this.sortOrder === unsorted || this.sortOrder === sortedDesc) {
+      this.items.sort((a, b) => a.text.localeCompare(b.text));
+      this.sortOrder = sortedAsc;
+    } else {
+      this.items.reverse();
+      this.sortOrder = sortedDesc;
+    }
+    return this.items;
   }
 }
