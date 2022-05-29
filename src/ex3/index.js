da9d176cccc0e7a9929f35d512c8c1ad9109e058
hhasona <hhasona@gmail.com>
2022-05-29 18:39:05 +0300

    adds new pokemon function

--- src/ex3/index.js
+++ src/ex3/index.js
@@ -1,2 +1,30 @@
-console.log("Starting exercise")
+import chalk from "chalk"
+import { Command } from "commander"
+import fetch from "node-fetch"
+const program = new Command()
+import dotenv from "dotenv"
+import { PokemonClient } from "./PokemonClient.js"
+import { ItemManager } from "./itemManager.js"
+dotenv.config()
 
+const pokemonClient = new PokemonClient()
+
+async function addNewPokemon(id) {
+  try {
+    const queryString = `${id}`
+    const requestUrl = `${pokemonClient.api}${queryString}`
+    const response = await fetch(requestUrl)
+    const pokemonData = await response.json()
+    return pokemonData.forms[0].name
+  } catch (err) {
+    throw err
+  }
+}
+
+function getCommanderProgram() {
+  const program = new Command()
+  return program
+}
+
+const programm = getCommanderProgram()
+programm.parse()
