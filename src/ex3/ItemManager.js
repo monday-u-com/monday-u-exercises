30bb26f8dd956399bc034eb82b9207fe99e41ab8
Barak Maron <Barak062@gmail.com>
2022-05-29 15:19:27 +0300

    feat: adds ex2 project and node init

---
+++ src/ex3/ItemManager.js
@@ -0,0 +1,132 @@
+class ItemManager {
+    constructor() {
+        this.tasks = [];
+        this.pokemon_client = new PokemonClient();
+        this.pokemons = [];
+    }
+    
+    /**
+     * init pokemon names array
+     */
+    async init()
+    {
+        const pokemons_names_response = await this.pokemon_client.GetPokemonsNames();
+        this.ParsePokemonNamesResponse(pokemons_names_response);
+    }
+
+    /**
+     * gets string and checks what case it is 
+     * @param {string} task_text string from input
+     */
+    async AddTask(task_text) {
+        // parse text to int
+        const pokemon_id = parseInt(task_text);
+        if (task_text.indexOf(',') > -1) // check if text has commas
+        {
+            await this.GetMultiplePokemons(task_text);
+        }
+        else if (Number.isInteger(pokemon_id)) // check if its number
+        {
+            await this.GetOnePokemon(pokemon_id);
+        }
+        else // regular task or pokemon name
+            this.tasks.push(task_text);
+    }
+    /**
+     * removes a task from array
+     * @param {int} task_to_remove_id task location in array
+     */
+    RemoveTask(task_to_remove_id) {
+        this.tasks.splice(task_to_remove_id, 1);
+    }
+
+    /**
+     * send id to pokemon client and insert it to tasks array
+     * @param {int} id 
+     */
+    async GetOnePokemon(id) {
+        // send the id to pokemon client
+        const result = Promise.resolve(this.pokemon_client.GetPokemonById(id));
+        // wait for response
+        await result;
+        result.then((res) => {
+            this.InsertResultToArray(res);
+        });
+    }
+
+    /**
+     * parse string from input send it to pokemon client and insert it to tasks array
+     * @param {string} ids string from input 
+     */
+    async GetMultiplePokemons(ids) {
+        const pokemon_ids = ids.split(',');
+        const result = Promise.resolve(this.pokemon_client.GetPokemonsByList(pokemon_ids));
+        await result;
+        result.then((all_data) => {
+            // run on all fetch results
+            all_data.forEach(res => {
+                this.InsertResultToArray(res);
+            });
+        });
+    }
+
+    /**
+     * inserts result to 
+     * @param {string} result 
+     * @param {int} id 
+     */
+    InsertResultToArray(result) {
+        // check if found pokemon
+        if (result.name !== "Error")
+        {
+            if(!this.CheckIfPokemonExists(result.id))
+                this.tasks.push({name: result.name, images: result.sprites, id: result.id});// found pokemon add the name and id of pokemon 
+        }
+        else
+            this.tasks.push({data: result.message, id: "Error"});// did not find pokemon 
+    }
+
+    /**
+     * clear all tasks
+     */
+    ClearArray() {
+        this.tasks = [];
+    }
+
+    /**
+     * sort tasks array 
+     */
+    SortArrayByName() {
+        this.tasks.sort((a, b) => {
+            // tasks object has name or data for error
+            const item1 = a.name || a.data;
+            const item2 = b.name || b.data;
+            return item1.toLowerCase().localeCompare(item2.toLowerCase());
+        });
+    }
+    
+    /**
+     * check if pokemon is in tasks
+     * @param {int} id id of pokemon to find
+     * @returns true if found false if not found
+     */
+    CheckIfPokemonExists(id)
+    {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    /**
+     * parses response into pokemons array
+     * @param {Array} response 
+     */
+    ParsePokemonNamesResponse(response)
+    {
+        if(response.name !== "Error")
+            response.forEach((pokemon) => {
+                // split url string and get rid of empty elements
+                const parsed_url = pokemon.url.split('/').filter((item) => item); 
+                const id = parsed_url[parsed_url.length - 1];
+                this.pokemons.push({name: pokemon.name, id});
+            });
+    }
+}
---
+++ src/ex3/PokemonClient.js
@@ -0,0 +1,76 @@
+
+class PokemonClient {
+    constructor() {
+        this.base_url = "https://pokeapi.co/api/v2/pokemon/";
+        this.pokemons_url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    }
+
+    async GetPokemonsNames()
+    {
+        const req = this.FetchAllPokemonsNamesRequest();
+        return Promise.resolve(req);
+    }
+    /**
+     * send one request to pokemon api
+     * @param {int} id pokemon id 
+     * @returns response from pokemon api
+     */
+    async GetPokemonById(id) {
+        const req = this.FetchRequestById(id);
+        return Promise.resolve(req);
+    }
+    /**
+     * sends parallel requests to pokemon api
+     * @param {array[string]} ids array of ids
+     * @returns list of responses from pokemon api
+     */
+    async GetPokemonsByList(ids) {
+        const requests = [];
+        ids.forEach(async (id) => {
+            const req = this.FetchRequestById(id);
+            requests.push(req);
+        });
+        return Promise.all(requests);
+    }
+    /**
+     * send a get request to pokemon api with id parse result
+     * @param {string} id id to 
+     * @returns {string} parsed result from api
+     */
+    async FetchRequestById(id)
+    {
+        return fetch(this.base_url.concat(id)).then(async (response) => {
+            try {
+                // check if response is valid
+                if (!response.ok) {
+                    throw new Error(`Pokemon with ID ${id} was not found`);
+                }
+                // parse response to json object
+                const res_obj = await response.json();
+                return res_obj;
+            }
+            catch (error) {
+                return error;
+            }
+        });
+    }
+
+    async FetchAllPokemonsNamesRequest()
+    {
+        return fetch(this.pokemons_url).then(async (response) => {
+            try {
+                // check if response is valid
+                if (!response.ok) {
+                    throw new Error(`Cant get pokemons names from API.`);
+                }
+                // parse response to json object
+                const res_obj = await response.json();
+                return res_obj.results;
+            }
+            catch (error) {
+                return error;
+            }
+        });
+    }
+}
+
