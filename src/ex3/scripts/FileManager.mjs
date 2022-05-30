f128c1a0bc4a52c0a6a8b80d6ddb14c2f2dd2a6b
Barak Maron <Barak062@gmail.com>
2022-05-30 21:25:36 +0300

    feat: adds dotenv file to project

--- src/ex3/scripts/FileManager.mjs
+++ src/ex3/scripts/FileManager.mjs
@@ -1,13 +1,18 @@
 import fs from 'fs/promises';
+import dotenv from 'dotenv';
 
 export default class FileManager {
+    constructor(){
+        dotenv.config();
+        this.file = process.env.FILE_DIRECTORY;
+    }
     /**
      * write task array to file in json format
      * @param {Array} tasks tasks array
      */
     async WriteToFileTasksArray(tasks) {
         const task_as_json = await JSON.stringify(tasks);
-        fs.writeFile('tasks.txt', task_as_json, 'utf-8');
+        fs.writeFile(this.file, task_as_json, 'utf-8');
     }
 
     /**
@@ -15,7 +20,7 @@ export default class FileManager {
      * @returns json object if file is full else empty array
      */
     async ReadFromFileTasks() {
-        const file_tasks = await fs.readFile('tasks.txt', 'utf8');
+        const file_tasks = await fs.readFile(this.file, 'utf8');
         if (file_tasks.length) {
             const json_file_object = await JSON.parse(file_tasks);
             return json_file_object;
