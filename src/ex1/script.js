05b9a7fd303bf9e6f161d4877c98101a3fd400d6
Anna Shishkova <asshishkova@gmail.com>
2022-05-30 15:52:02 +0300

    Use base class name and use it across all the buttons

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -51,7 +51,7 @@ function createListElement(todoText, animation) {
   }
   setTimeout (() => { listItem.className = "existing-item";}, 700);
   listItem.innerHTML = `<div class="todo-text">${todoText}</div>
-                        <button class="remove-todo-button"><i class="fa fa-trash"></i></button>`;
+                        <button class="remove-todo-button btn-hover"><i class="fa fa-trash"></i></button>`;
   return listItem;
 }
 
