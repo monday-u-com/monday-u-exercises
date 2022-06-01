9613ad9f961b08cf003f1ac6a95f0d8d8230ec90
hhasona <hhasona@gmail.com>
2022-06-01 14:24:09 +0300

    deletes items by index

--- src/ex3/index.js
+++ src/ex3/index.js
@@ -1,24 +1,22 @@
 import fetch from "node-fetch"
 import { Command } from "commander"
 import PokemonClient from "./PokemonClient.js"
-import ItemManager from "./itemManager.js"
 import fs from "fs"
 
 const pokemonClient = new PokemonClient()
 
 async function getPokemon(id) {
-  fs.readFileSync("./pokemon.txt", "utf8", (err, data) => {
-    if (err) {
-      console.log(data.toString())
-      throw err
-    }
-  })
-  const response = await fetch(`${pokemonClient.api}${id}`)
-  const json = await response.json()
-  return json
+  const isInteger = parseInt(id)
+  if (isInteger) {
+    const response = await fetch(`${pokemonClient.api}${id}`)
+    const json = await response.json()
+    return json
+  } else {
+    return id
+  }
 }
 
-async function getAllItemsFromFile() {
+function getAllItemsFromFile() {
   const items = fs.readFileSync("./pokemon.txt", "utf8", (err, data) => {
     if (err) {
       console.log(data.toString())
@@ -28,25 +26,45 @@ async function getAllItemsFromFile() {
   return items
 }
 
+function deleteItemByIndex(index) {
+  const items = getAllItemsFromFile()
+  const newItems = items.split("\n")
+  newItems.splice(index, 1)
+  fs.writeFileSync("./pokemon.txt", newItems.join("\n"))
+}
+
 function getCommanderProgram() {
   const program = new Command()
   program.name("pokemon-cli").version("0.0.1").description("Pokemon CLI")
 
   program
     .command("add <name>")
-    .description("Add a pokemon to your collection")
+    .description("Add a new todo task")
     .action(async (id) => {
       const pokemon = await getPokemon(id)
-      fs.appendFileSync("./pokemon.txt", `Catch ${pokemon.name}\n`)
-      console.log("New todo added successfully")
+      if (pokemon.name) {
+        fs.appendFileSync("./pokemon.txt", `Catch ${pokemon.name}\n`)
+        console.log("New todo added successfully")
+      } else {
+        fs.appendFileSync("./pokemon.txt", `${id}\n`)
+        console.log("New todo added successfully")
+      }
     })
 
   program
-    .command("getAll")
+    .command("get")
     .description("Get all pokemon")
     .action(async () => {
       const items = await getAllItemsFromFile()
       console.log(items)
+    })
+
+  program
+    .command("delete <index>")
+    .description("Delete a pokemon")
+    .action(async (index) => {
+      deleteItemByIndex(index)
+      console.log("todo delete successfully")
     })
 
   return program
