e7e5424bebf0d28aa084ecda475fb40c2e9272da
Kuper-S <kupkup1992@gmail.com>
2022-06-02 12:05:52 +0300

    add an done and todo text file ans list it

--- src/ex3/CLI_APP/index.js
+++ src/ex3/CLI_APP/index.js
@@ -61,3 +61,37 @@ $ node index.js report	 #Statistics`;
 
 	console.log(UsageText);
 };
+
+const listFunction = () => {
+
+	// Create a empty array
+	let data = [];
+    
+	// Read from todo.txt and convert it into a string
+	const fileData = readFileSync(currentWorkingDirectory +
+			'todo.txt').toString();
+
+	// Split the string and store into array
+	data = fileData.split('\n');
+
+	// Filter the string for any empty lines in the file
+	let filterData = data.filter(function(value) {
+		return value !== '';
+	});
+
+	if (filterData.length === 0) {
+		console.log(chalk.bgYellow('There are no pending todos!'));
+	}
+	for (let i = 0; i < filterData.length; i++) {
+		console.log((filterData.length - i) + '. ' +
+			filterData[i]);
+	}
+};
+
+// Check if user input include numbers or not .
+const inputOfNums = (inputValue) => {
+    const regex = /^[\d,]+$/;
+    return regex.test(inputValue);
+  }
+
+
