c58242747aa2e8d38082a92fbe39e47784aaf67a
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-06-15 09:15:30 +0300

    feat: in-memory cache

--- src/ex4/server/clients/pokemon_client.js
+++ src/ex4/server/clients/pokemon_client.js
@@ -1,32 +1,38 @@
 // The Pokemon Client (using axios) goes here
 const axios = require('axios');
+const cache = require('memory-cache');
 const URL = `https://pokeapi.co/api/v2/pokemon/`;
+const TTL = 600000;//10 min
 
 async function catchPokemons(ids) {
     try {
         console.log('ids:', ids);
-        const promises = ids.map((id) => {
-            return axios.get(URL + id)
+        let res = [];
+        let promises = ids.map((id) => {
+            const cached = cache.get(id);
+            if (!cached)
+                return axios.get(URL + id);
+            else {
+                console.log('cache hit!');
+                res.push(cached);
+            }
         });
-        console.log('promises:', promises);
-        const res = [];
-        await axios.all([...promises])
-            .then(axios.spread((...data) => {
-                data.forEach((element) => {
-                    console.log(element.data.name);
-                    res.push(element.data);
-                })
-            }))
+        promises = promises.filter((x) => x !== undefined);
+        if (promises.length > 0)
+            await axios.all([...promises])
+                .then(axios.spread((...data) => {
+                    data.forEach((element) => {
+                        console.log(element.data.name);
+                        cache.put((element.data.id).toString(), element.data, TTL);
+                        res.push(element.data);
+                    })
+                }))
         return res;
     }
     catch (err) {
-        // console.log(err);
         console.log('catch pokemon failed', err.message);
     }
-
 }
-
-// catchPokemons(1);
 
 module.exports = {
     catchPokemons,
