95d2434d73c7526ec10a43571769e44a50cb59d5
liortlv <99750449+liortlv@users.noreply.github.com>
2022-06-15 20:52:57 +0300

    Add all routes to api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,49 +1,41 @@
 // Define your endpoints here (this is your "controller file")
 const express = require("express");
-const itemManagerObj = require("../services/item-manager");
+const taskManager = require("../services/task-manager.js");
 const fs = require("fs");
 
-const itemManager = new itemManagerObj.ItemManager();
 const router = express.Router();
 
-router.get("/getAll", async (req, res) => {
-   const TASKS_FILE_NAME = "./server/tasks.json";
-   if (!fs.existsSync(TASKS_FILE_NAME)) {
-      fs.writeFileSync(TASKS_FILE_NAME, JSON.stringify([]));
-   } else {
-      const fileData = await fs.promises.readFile(TASKS_FILE_NAME);
-      itemManager.items = JSON.parse(fileData);
-      itemManager.pendingTasks = itemManager.items.length;
-   }
-   res.status(200).json(itemManager);
+router.get("/getAll", (req, res) => {
+   res.status(200).json(taskManager.getTasks());
 });
 
-router.delete("/:id", async (req, res) => {
-   itemManager.remove(req.params.id);
-   res.status(200).json(itemManager);
+router.post("/add", (req, res) => {
+   console.log(req.body);
+   taskManager.add(req.body);
+   res.status(200).json(taskManager.getTasks());
+});
+
+router.delete("/del", (req, res) => {
+   taskManager.clear();
+   res.status(200).json(taskManager.getTasks());
+});
+
+router.delete("/del/:id", (req, res) => {
+   let id = Number.parseInt(req.params.id);
+   if (isNaN(id)) {
+      let error = Error();
+      error.statusCode = 400;
+      error.message = "wrong parameters";
+      throw error;
+   }
+   taskManager.remove(id);
+   res.status(200).json(taskManager.getTasks());
+});
+
+router.get("/sort/:way", (req, res) => {
+   let way = parseInt(req.params.way);
+   taskManager.sort(way);
+   res.status(200).json(taskManager.getTasks());
 });
 
 module.exports = router;
-
-// router.post("/", newTask);
-// router.delete("/:id", deleteTask);
-
-// async function createJedi(req, res) {
-//    console.log("saving jedi");
-//    await jediService.addJedi(req.body);
-//    res.status(200).json(req.body);
-// }
-
-// async function deleteJedi(req, res) {
-//    let jediId = Number.parseInt(req.params.id);
-//    //TODO 4. Turn it to error with proper status and throw it
-//    if (isNaN(jediId)) {
-//       let error = Error();
-//       error.statusCode = 400;
-//       error.message = "wrong parameters";
-//       throw error;
-//    }
-
-//    const data = await jediService.deleteJedi(jediId);
-//    res.status(200).json(data);
-// }
