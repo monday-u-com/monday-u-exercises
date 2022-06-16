eef73a41a1f3936eba683d55b491ddf7c84a43e0
Barak Maron <Barak062@gmail.com>
2022-06-17 02:23:02 +0300

    feat: adds clients

---
+++ src/ex4/server/clients/PokemonClient.mjs
@@ -0,0 +1,25 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+export default class PokemonClient {
+  constructor() {
+    this.base_url = 'https://pokeapi.co/api/v2/pokemon/';
+    this.pokemons_url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+  }
+
+  /**
+  * send a get request to pokemon api with id parse result
+  * @param {string} id id
+  * @returns {string} parsed result from api
+  */
+  async FetchRequestById(id) {
+    return axios.get(this.base_url.concat(id)).then((response) => {     
+     return response.data;
+    });
+  }
+
+  async FetchAllPokemonsNamesRequest() {
+    return axios.get(this.pokemons_url).then((response) => {     
+     return response.data.results;
+    });
+  }
+}
