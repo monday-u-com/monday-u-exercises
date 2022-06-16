7709ed7d7aaa07f4c76761cc9a69febb63cd405b
Barak Maron <Barak062@gmail.com>
2022-06-17 02:18:38 +0300

    feat: adds global html vars

---
+++ src/ex4/dist/config/GlobalConses.js
@@ -0,0 +1,19 @@
+export const TASK_HTML = `<li class="todo_task animate__animated animate__fadeIn">
+                            <span class="task_number"></span>
+                            <span class="task_text"></span>
+                            <div class="image_sprites">
+                            </div>
+                            <div class="task_buttons">
+                                <button class="delete_task_button" id="">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </button>
+                                <button class="complete_task_button">
+                                    <i class="fa-solid fa-check"></i>
+                                </button>
+                            </div>
+                        </li>`;
+export const POKEMON_IMAGE_DIV = '<div class="image_sprites"></div>';
+
+export const LOADER = '<div class="spinner_container"><div class="loader"></div></div>';
+
+export const ERROR = '<div class="error"></div>';
