067a08743e944cebffad91174a823be842abfb22
hhasona <hhasona@gmail.com>
2022-06-17 14:04:50 +0300

    creates boilierplates

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -1,1 +1,15 @@
 // Express boilerplate, hosting the `dist` file, connecting to the routes
+const express = require('express');
+require('express-async-errors');
+const port = 8080;
+const app = express();
+
+
+app.use(express.static('dist'));
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+  console.log("Server started on port", port);
+});
