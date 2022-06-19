56ae1921bf51b1aac71fb276a78b7db022705061
liortlv <99750449+liortlv@users.noreply.github.com>
2022-06-19 15:03:46 +0300

    Add Task db table/model and migration file

---
+++ server/db/migrations/20220619115747-create-task.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      pokemonID: {
+        type: Sequelize.INTEGER
+      },
+      pokemonName: {
+        type: Sequelize.STRING
+      },
+      pokemonType: {
+        type: Sequelize.STRING
+      },
+      imageURL: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
