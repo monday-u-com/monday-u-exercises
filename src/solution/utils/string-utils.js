62fcb0384f533a70989324936bf7f66c3eec84a2
adihod1 <didah3@gmail.com>
2022-06-24 00:47:45 +0300

    fix: string utils file export

--- src/solution/utils/string-utils.js
+++ src/solution/utils/string-utils.js
@@ -1,11 +1,16 @@
-    
-export function capitalizeText(text) {
+
+function capitalizeText(text) {
     return text
         .split(' ')
         .map((s) => s[0].toUpperCase() + s.slice(1))
         .join(' ');
 }
 
-export function generateUniqueID() { 
+function generateUniqueID() {
     return Math.random().toString(36).substr(2, 9);
+}
+
+module.exports = {
+    capitalizeText,
+    generateUniqueID
 }
