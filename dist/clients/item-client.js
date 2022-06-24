de2788e60666bb039c945bc76feadc5d071d4b4f
liortlv <99750449+liortlv@users.noreply.github.com>
2022-06-24 20:09:44 +0300

    Refactor organize

--- dist/clients/item-client.js
+++ dist/clients/item-client.js
@@ -1,8 +1,8 @@
 export default new (class ItemClient {
    async getAllTasks() {
       const response = await fetch("/getAll");
-      const tasks = await response.json();
-      return tasks;
+
+      return await response.json();
    }
 
    async deleteTask(id) {
@@ -16,6 +16,7 @@ export default new (class ItemClient {
          body: JSON.stringify({ task }),
       };
       const response = await fetch("/add", reqOptions);
+
       return await response.json();
    }
 
@@ -25,8 +26,8 @@ export default new (class ItemClient {
 
    async sortTasks(way) {
       const response = await fetch(`/sort/${way}`);
-      const tasks = await response.json();
-      return tasks;
+
+      return await response.json();
    }
 
    async checkMarkTask(isChecked, taskID) {
