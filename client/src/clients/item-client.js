da1658b2160b7ab7f3c4e0b8393dace126bf37ee
liortlv <99750449+liortlv@users.noreply.github.com>
2022-06-27 13:34:22 +0300

    add item-client api file

---
+++ client/src/clients/item-client.js
@@ -0,0 +1,41 @@
+export default new (class ItemClient {
+   async getAllTasks() {
+      const response = await fetch("/getAll");
+
+      return await response.json();
+   }
+
+   async deleteTask(id) {
+      await fetch(`/del/${id}`, { method: "DELETE" });
+   }
+
+   async addTask(task) {
+      const reqOptions = {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({ task }),
+      };
+      const response = await fetch("/add", reqOptions);
+
+      return await response.json();
+   }
+
+   async clearTasks() {
+      await fetch("/del", { method: "DELETE" });
+   }
+
+   async sortTasks(way) {
+      const response = await fetch(`/sort/${way}`);
+
+      return await response.json();
+   }
+
+   async checkMarkTask(isChecked, taskID) {
+      const reqOptions = {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({ isChecked: isChecked, taskID: taskID }),
+      };
+      await fetch("/checkmark", reqOptions);
+   }
+})();
