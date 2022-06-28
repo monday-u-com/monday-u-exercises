8133c0ad17814c7ccd85998a8e6c063afe2ef757
liortlv <99750449+liortlv@users.noreply.github.com>
2022-06-28 15:33:17 +0300

    Fix pokemon image to be inside task

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -3,14 +3,16 @@ import Checkbox from "./Checkbox";
 
 function Task({ task }) {
    const addPokemonImage = () => {
-      return <img src="task.imageURL" className="pokemon-image" alt="pokemon image" />;
+      return <img src={task.imageURL} className="pokemon-image" alt="pokemon image" />;
    };
 
    return (
       <li data-id={task.id} className="task-container">
          <Checkbox />
-         <div className="task">{task.text}</div>
-         {task.imageURL ? addPokemonImage() : ""}
+         <div className="task">
+            {task.text}
+            {task.imageURL ? addPokemonImage() : ""}
+         </div>
       </li>
    );
 }
