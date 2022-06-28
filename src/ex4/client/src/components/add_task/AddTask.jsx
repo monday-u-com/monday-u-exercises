9beaef6e6112c0df886678558c1035d8857fbce7
Alex Levenberg <zeronid@gmail.com>
2022-06-28 13:07:17 +0300

    FIX: input new item UI

--- src/ex4/client/src/components/add_task/AddTask.jsx
+++ src/ex4/client/src/components/add_task/AddTask.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './AddTask.css'
 
 function AddTask({ children }) {
     return (
-        <div className='add-task-container'>
-            <input></input>
-            <button>+</button>
+        <div className={'add-task-container'}>
+            <input className={'add-task-input'}></input>
+            <button className={'add-task-button'}>+</button>
         </div >
     )
 }
