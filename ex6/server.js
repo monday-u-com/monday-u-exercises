5cfb2fcb65adbe8e984560449f5598f8fbb26d97
Michay Kalika <michaykalika@Michays-MacBook-Pro.local>
2022-07-01 13:44:59 +0300

    task done

--- ex6/server.js
+++ ex6/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const api = require('./server/routes/api');
+const cors = require('cors');
 
 const main = async () => {
 	const app = express();
-
-	app.use(express.static(path.join(__dirname, 'dist')));
-
+	app.use(cors());
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 
