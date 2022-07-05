87e263a9224afc001ae20948e993c3e61f245b1e
Kuper-S <kupkup1992@gmail.com>
2022-07-05 09:20:24 +0300

    add ex7 folders and files

---
+++ src/src/ex7/client/src/reducers/items-view-reducer.js
@@ -0,0 +1,12 @@
+import actionTypes from "../actions/constants";
+
+const initialState = {
+};
+
+const itemsViewReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default itemsViewReducer;
