3ac22f0dbb878fd42f2fbd59b42192179925200f
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-07 16:00:20 +0300

    Add redux logger middleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
+import logger from "redux-logger";
 import allReducers from "./reducers";
 
 export const store = configureStore({
    reducer: allReducers,
-   middleware: [thunkMiddleware],
+   middleware: [thunkMiddleware, logger],
    preloadedState: {},
 });
