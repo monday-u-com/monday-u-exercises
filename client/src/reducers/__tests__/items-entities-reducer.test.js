b0791ff5e1ec68ba166d2c6003973bb5d91c01f5
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-10 12:34:34 +0300

    Test item entities reducer

---
+++ client/src/reducers/__tests__/items-entities-reducer.test.js
@@ -0,0 +1,83 @@
+import itemsEntitiesReducer from "../items-entities-reducer";
+import actionTypes from "../../actions/constants";
+
+test("should return the initial state", () => {
+   expect(itemsEntitiesReducer(undefined, { type: undefined })).toEqual({ tasks: [] });
+});
+
+test("should handle a task being added to an empty list", () => {
+   const previousState = { tasks: [] };
+   const action = {
+      type: actionTypes.ADD_TASK_FULFILLED,
+      payload: [{ id: 1, text: "A todo" }],
+   };
+
+   expect(itemsEntitiesReducer(previousState, action)).toEqual({
+      tasks: [{ id: 1, text: "A todo" }],
+   });
+});
+
+test("should handle a task being added to an existing list", () => {
+   const previousState = {
+      tasks: [{ id: 1, text: "A todo" }],
+   };
+   const action = {
+      type: actionTypes.ADD_TASK_FULFILLED,
+      payload: [{ id: 2, text: "A new todo" }],
+   };
+
+   expect(itemsEntitiesReducer(previousState, action)).toEqual({
+      tasks: [
+         { id: 1, text: "A todo" },
+         { id: 2, text: "A new todo" },
+      ],
+   });
+});
+
+test("should clear all tasks from an existing list", () => {
+   const previousState = {
+      tasks: [
+         { id: 1, text: "A todo" },
+         { id: 2, text: "A new todo" },
+      ],
+   };
+   const action = {
+      type: actionTypes.CLEAR_TASKS_FULFILLED,
+   };
+
+   expect(itemsEntitiesReducer(previousState, action)).toEqual({
+      tasks: [],
+   });
+});
+
+test("should clear all tasks from an empty list", () => {
+   const previousState = {
+      tasks: [],
+   };
+   const action = {
+      type: actionTypes.CLEAR_TASKS_FULFILLED,
+   };
+
+   expect(itemsEntitiesReducer(previousState, action)).toEqual({
+      tasks: [],
+   });
+});
+
+test("should delete one task from an existing list", () => {
+   const previousState = {
+      tasks: [
+         { id: 1, text: "A todo" },
+         { id: 2, text: "A new todo" },
+      ],
+   };
+   const action = {
+      type: actionTypes.DELETE_TASK_FULFILLED,
+      meta: {
+         arg: 1,
+      },
+   };
+
+   expect(itemsEntitiesReducer(previousState, action)).toEqual({
+      tasks: [{ id: 2, text: "A new todo" }],
+   });
+});
