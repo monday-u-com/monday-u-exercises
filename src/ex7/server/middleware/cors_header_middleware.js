0468acc8951977b094cc39b1cb4f6c1083b07a31
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-07-11 03:14:10 +0300

    update main to ex6 version

---
+++ src/ex7/server/middleware/cors_header_middleware.js
@@ -0,0 +1,12 @@
+module.exports = function corsHeader(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+};
