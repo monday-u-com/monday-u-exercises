ec8d69b67d6840aa0d6479de9682f71bc3ace1fe
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-13 12:46:53 +0300

    Add snapshot tests

--- client/src/components/Task/__tests__/Task.text.jsx
+++ client/src/components/Task/__tests__/Task.text.jsx
@@ -1,7 +1,7 @@
 import Task from "../Task";
 import ShallowRenderer from "react-test-renderer/shallow";
 
-it("renders correctly", () => {
+it("renders pokemon task correctly with true status", () => {
    const renderer = new ShallowRenderer();
 
    const task = {
@@ -15,12 +15,39 @@ it("renders correctly", () => {
    expect(tree).toMatchSnapshot();
 });
 
-it("renders correctly", () => {
+it("renders pokemon task correctly with false status", () => {
+   const renderer = new ShallowRenderer();
+
+   const task = {
+      id: 3,
+      status: false,
+      text: "Catch Bulbasaur of type Grass,poison",
+      imageURL: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+   };
+
+   const tree = renderer.render(<Task task={task} />);
+   expect(tree).toMatchSnapshot();
+});
+
+it("renders regular task correctly without status", () => {
    const renderer = new ShallowRenderer();
 
    const task = {
       id: 3,
       text: "Hello",
+   };
+
+   const tree = renderer.render(<Task task={task} />);
+   expect(tree).toMatchSnapshot();
+});
+
+it("renders regular task correctly with status true", () => {
+   const renderer = new ShallowRenderer();
+
+   const task = {
+      id: 3,
+      text: "Hello",
+      status: true,
    };
 
    const tree = renderer.render(<Task task={task} />);
