6e34e9b789e22cd350587af26be765ab03eb6c16
Aviranisa <aviranisa@gmail.com>
2022-07-14 19:42:53 +0300

    build fixes

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -7,7 +7,7 @@ const todoRouter = require("./server/routes/api.js");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(cors());
 app.use("/tasks", todoRouter);
 
