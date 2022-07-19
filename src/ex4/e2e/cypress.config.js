3b4374898250f6292342a58aeaf0ac636f5554c8
Aviranisa <aviranisa@gmail.com>
2022-07-19 16:17:17 +0300

    ex_8 remove bulid folder

--- src/ex4/e2e/cypress.config.js
+++ src/ex4/e2e/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-  },
+	e2e: {
+		baseUrl: "http://localhost:2000",
+	},
 });
--- src/ex4/e2e/cypress/e2e/add-todo.cy.js
+++ src/ex4/e2e/cypress/e2e/add-todo.cy.js
@@ -1,9 +1,17 @@
 describe("Add Todo Action", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
+	beforeEach(() => {
+		cy.visit("http://localhost:2000");
+	});
 
-  it("Should add a new todo", () => {
-    // TODO: fill this test
-  });
+	it("Should add a new todo", () => {
+		cy.get("#root > div > div:nth-child(2) > div:nth-child(2) > img").click();
+
+		cy.get("#root > div > div.margin-bottom-element > div > input").type(
+			"1,2,3"
+		);
+
+		cy.get("#root > div > div.margin-bottom-element > div > div > img").click();
+
+		// TODO: fill this test
+	});
 });
