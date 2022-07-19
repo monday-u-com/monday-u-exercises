d0c07754b9b915ad1ef0c8e3579b0cb15929cf7f
Aviranisa <aviranisa@gmail.com>
2022-07-19 18:15:53 +0300

    ex_8 cypress

--- src/ex4/e2e/cypress/e2e/add-todo.cy.js
+++ src/ex4/e2e/cypress/e2e/add-todo.cy.js
@@ -3,15 +3,21 @@ describe("Add Todo Action", () => {
 		cy.visit("http://localhost:2000");
 	});
 
-	it("Should add a new todo", () => {
+	after(() => {
 		cy.get("#root > div > div:nth-child(2) > div:nth-child(2) > img").click();
+	});
 
+	it("Should add a new 3 pokemons", () => {
 		cy.get("#root > div > div.margin-bottom-element > div > input").type(
 			"1,2,3"
 		);
-
 		cy.get("#root > div > div.margin-bottom-element > div > div > img").click();
+	});
 
-		// TODO: fill this test
+	it("Should add a new regular task", () => {
+		cy.get("#root > div > div.margin-bottom-element > div > input").type(
+			"Do the dishes"
+		);
+		cy.get("#root > div > div.margin-bottom-element > div > div > img").click();
 	});
 });
