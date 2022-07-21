72067aa57a60f5c330f20591a52f730e0f5bda03
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-21 19:45:50 +0300

    Refactor test to use class consts

--- e2e/cypress/e2e/add-todo.cy.js
+++ e2e/cypress/e2e/add-todo.cy.js
@@ -1,32 +1,35 @@
+const HOMECARD_CLEAR_ALL_CLASS_NAME = ".HomeCard_clear-all__Rq0JP";
+const INPUT_CLASS_NAME = "input#AddBar_task-text__MoHCu";
+const ADD_BUTTON_CLASS_NAME = "Button.AddBar_add-task__B1Il6";
+const ALL_TASKS_CONTAINER_CLASS_NAME = ".TaskList_all-tasks-container__7j9ml";
+
 describe("Add Task Action", () => {
    beforeEach(() => {
       cy.visit("http://localhost:3000");
-      cy.get(".HomeCard_clear-all__Rq0JP").click();
+      cy.get(HOMECARD_CLEAR_ALL_CLASS_NAME).click();
    });
 
    it("Should add a new regular task using the add button", () => {
       const taskText = "A regular task using the btn";
 
-      cy.get("input#AddBar_task-text__MoHCu").type(taskText);
-      cy.get("Button.AddBar_add-task__B1Il6").click();
-      cy.get(".TaskList_all-tasks-container__7j9ml").contains(taskText).should("be.visible");
+      cy.get(INPUT_CLASS_NAME).type(taskText);
+      cy.get(ADD_BUTTON_CLASS_NAME).click();
+      cy.get(ALL_TASKS_CONTAINER_CLASS_NAME).contains(taskText).should("be.visible");
    });
 
    it("Should add a new regular task using the enter key", () => {
       const taskText = "A regular task using enter key";
 
-      cy.get("input#AddBar_task-text__MoHCu").type(taskText).type("{enter}");
-      cy.get(".TaskList_all-tasks-container__7j9ml").contains(taskText).should("be.visible");
+      cy.get(INPUT_CLASS_NAME).type(taskText).type("{enter}");
+      cy.get(ALL_TASKS_CONTAINER_CLASS_NAME).contains(taskText).should("be.visible");
    });
 
    it("Should add a new single pokemon task", () => {
       const taskText = "1";
       const taskFinishedText = "Catch Bulbasaur of type Grass,poison";
 
-      cy.get("input#AddBar_task-text__MoHCu").type(taskText).type("{enter}");
-      cy.get(".TaskList_all-tasks-container__7j9ml")
-         .contains(taskFinishedText)
-         .should("be.visible");
+      cy.get(INPUT_CLASS_NAME).type(taskText).type("{enter}");
+      cy.get(ALL_TASKS_CONTAINER_CLASS_NAME).contains(taskFinishedText).should("be.visible");
    });
 
    it("Should add a multiple pokemons tasks", () => {
@@ -35,8 +38,8 @@ describe("Add Task Action", () => {
       const taskFinishedText2 = "Catch Charmeleon of type Fire";
       const taskFinishedText3 = "Catch Charizard of type Fire,flying";
 
-      cy.get("input#AddBar_task-text__MoHCu").type(taskText).type("{enter}");
-      cy.get(".TaskList_all-tasks-container__7j9ml")
+      cy.get(INPUT_CLASS_NAME).type(taskText).type("{enter}");
+      cy.get(ALL_TASKS_CONTAINER_CLASS_NAME)
          .should("contain", taskFinishedText1)
          .and("contain", taskFinishedText2)
          .and("contain", taskFinishedText3);
@@ -46,7 +49,7 @@ describe("Add Task Action", () => {
       const taskText = "9999";
       const taskFinishedText = "Pokemon ID 9999 does not exist";
 
-      cy.get("input#AddBar_task-text__MoHCu").type(taskText).type("{enter}");
-      cy.get(".TaskList_all-tasks-container__7j9ml").should("contain", taskFinishedText);
+      cy.get(INPUT_CLASS_NAME).type(taskText).type("{enter}");
+      cy.get(ALL_TASKS_CONTAINER_CLASS_NAME).should("contain", taskFinishedText);
    });
 });
