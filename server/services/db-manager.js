2eeccf632c4883d13618d72b57427d574eb9a733
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-15 16:34:53 +0300

    Change "db" folder name to "storage"

--- server/services/db-manager.js
+++ server/services/db-manager.js
@@ -1,58 +1,58 @@
-const { Task } = require("../db/models");
+const { Task } = require("../storage/models");
 
 class DBManager {
-   getAllTasks = async () =>
-      await Task.findAll({
-         where: {
-            deleted: null,
-         },
+  getAllTasks = async () =>
+    await Task.findAll({
+      where: {
+        deleted: null,
+      },
+    });
+
+  async addTask(task) {
+    const response = await Task.bulkCreate(task);
+    await Task.sync();
+
+    return response;
+  }
+
+  async deleteTask(id) {
+    await Task.findOne({ where: { id: id } }).then(async (task) => {
+      if (!task) {
+        throw new Error("No task found");
+      }
+      await task.update({ deleted: Date.now() });
+    });
+  }
+
+  clearTasks = async () => {
+    await Task.findAll({
+      where: {
+        deleted: null,
+      },
+    }).then((tasks) => {
+      tasks.forEach(async (task) => {
+        await task.update({ deleted: Date.now() });
       });
+    });
+  };
 
-   async addTask(task) {
-      const response = await Task.bulkCreate(task);
-      await Task.sync();
+  async checkMarkTask(isChecked, taskID) {
+    await Task.findOne({ where: { id: taskID } }).then(async (task) => {
+      if (!task) {
+        throw new Error("No task found");
+      }
+      await task.update({ status: isChecked });
+      await task.update({ done: isChecked ? Date.now() : null });
+    });
 
-      return response;
-   }
+    return await this.getAllTasks();
+  }
 
-   async deleteTask(id) {
-      await Task.findOne({ where: { id: id } }).then(async (task) => {
-         if (!task) {
-            throw new Error("No task found");
-         }
-         await task.update({ deleted: Date.now() });
-      });
-   }
-
-   clearTasks = async () => {
-      await Task.findAll({
-         where: {
-            deleted: null,
-         },
-      }).then((tasks) => {
-         tasks.forEach(async (task) => {
-            await task.update({ deleted: Date.now() });
-         });
-      });
-   };
-
-   async checkMarkTask(isChecked, taskID) {
-      await Task.findOne({ where: { id: taskID } }).then(async (task) => {
-         if (!task) {
-            throw new Error("No task found");
-         }
-         await task.update({ status: isChecked });
-         await task.update({ done: isChecked ? Date.now() : null });
-      });
-
-      return await this.getAllTasks();
-   }
-
-   async undoDelete() {
-      await Task.findOne({ order: [["deleted", "DESC"]] }).then(async (task) => {
-         await task.update({ deleted: null });
-      });
-   }
+  async undoDelete() {
+    await Task.findOne({ order: [["deleted", "DESC"]] }).then(async (task) => {
+      await task.update({ deleted: null });
+    });
+  }
 }
 
 module.exports = new DBManager();
