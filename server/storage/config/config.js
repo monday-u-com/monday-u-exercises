23fa42f30204ba59a5196ee329ab032268e79b52
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-15 16:40:01 +0300

    Modify storage config file to use local db for development and postgres for production

--- server/storage/config/config.js
+++ server/storage/config/config.js
@@ -1,26 +1,28 @@
 require("dotenv").config();
 module.exports = {
-   development: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "mysql",
-      pool: { max: 20 },
-   },
-   test: {
-      username: "root",
-      password: null,
-      database: "database_test",
-      host: "127.0.0.1",
-      dialect: "mysql",
-   },
-   production: {
-      username: "root",
-      password: null,
-      database: "database_production",
-      host: "127.0.0.1",
-      dialect: "mysql",
-   },
+  development: {
+    username: "root",
+    password: "password",
+    database: "todo_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
